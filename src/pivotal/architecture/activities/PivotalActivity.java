dbc26c9b388ad3c5cd51789f80ee5c20fbca7c47
Aaron Jarecki <aaron.jarecki@xtremelabs.com>
2013-11-20 02:26:14 -0500

    Initial Activity

---
+++ src/pivotal/architecture/activities/PivotalActivity.java
@@ -0,0 +1,51 @@
+package pivotal.architecture.activities;
+
+import pivotal.architecture.callbacks.PivotalLoaderCallbacksListener;
+import pivotal.architecture.loaders.PivotalPeopleViewLoaderCallbacks;
+import android.R;
+import android.app.Activity;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class PivotalActivity extends Activity implements PivotalLoaderCallbacksListener {
+
+	private ListView mListView;
+	private PivotalPeopleViewLoaderCallbacks mPeopleCursorLoader;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+		setContentView(R.layout.activity_pivotal);
+		super.onCreate(savedInstanceState);
+		mListView = (ListView) findViewById(R.id.activity_pivotal_list_view);
+		mPeopleCursorLoader = new PivotalPeopleViewLoaderCallbacks(getApplicationContext(), getLoaderManager(), this);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		mPeopleCursorLoader.onStart(getApplicationContext());
+	}
+
+	@Override
+	protected void onStop() {
+		mPeopleCursorLoader.onStop(getApplicationContext());
+		super.onStop();
+	}
+
+	@Override
+	public void onLoadFinished(final Uri uri, final Cursor cursor) {
+		Toast.makeText(getApplicationContext(), "We Got Data!", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onLoaderReset(final Loader<Cursor> loader) {
+		mListView.setAdapter(null);
+	}
+
+}
